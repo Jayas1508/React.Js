3025be0ec8a26ef75c16e110fce2a7837dc48a52
jay asodaria <162321772+Jayas1508@users.noreply.github.com>
2025-01-30 15:44:56 +0530

    Delete Project-10/Redux-api directory

--- Project-10/Redux-api/src/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import {thunk} from "redux-thunk";
-import reducer from "./Services/Reducers";
-const storeapi = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, storeapi(applyMiddleware(thunk)));
-export default store;
