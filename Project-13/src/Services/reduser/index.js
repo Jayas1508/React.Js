a4c960d039dcf53bd60daa97303a88f1fe8644d3
jay asodaria <162321772+Jayas1508@users.noreply.github.com>
2025-01-30 16:19:05 +0530

    Delete Project-13 directory

--- Project-13/src/Services/reduser/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux"
-import userReducer from "./userReducer"
-import { authReducer } from "./authReducer"
-
-const reducer = combineReducers({
-    userReducer,
-    authReducer
-})
-export default reducer
--- Project-13/src/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, compose, createStore } from "redux";
-import {thunk} from "redux-thunk";
-import reducer from "./Services/reduser";
-const storeapi = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, storeapi(applyMiddleware(thunk)));
-export default store;
